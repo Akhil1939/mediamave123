2350ad8c20489630d1ed4f37f59cf76d9d2c5cd7
akhil1939 <akhilkukadiya464@gmail.com>
2022-07-22 19:43:30 +0530

    commit-8

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if ( process.env.NODE_ENV == "production"){
 
   const path = require("path");
 
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
 
       res.sendFile(path.resolve(__dirname, 'ui', 'build', 'index.html'));
 
