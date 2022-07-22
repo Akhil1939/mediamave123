7431ac305bf2aeac394f62315c513f0807424236
akhil1939 <akhilkukadiya464@gmail.com>
2022-07-22 12:06:54 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,45 @@
+const dotenv = require('dotenv')
+const express = require("express");
+const app = express();
+const cookieParser = require('cookie-parser')
+
+const mongoose = require('mongoose');
+dotenv.config({path:'./config.env'})
+require('./db/conn')
+// const User = require('./model/userSchema')
+app.use(express.json());
+app.use(cookieParser())
+
+//router file for storing path
+app.use(require('./router/auth')); 
+
+const PORT = process.env.PORT  || 3030;
+
+// middleware()
+
+app.get("/", (req, res) => { 
+  res.send("HOME"); 
+});
+app.get("/login", (req, res) => {
+  res.send("login"); 
+});
+
+if ( process.env.NODE_ENV == "production"){
+
+  app.use(express.static("ui/build"));
+
+  // const path = require("path");
+
+  // app.get("*", (req, res) => {
+
+  //     res.sendFile(path.resolve(__dirname, 'ui', 'build', 'index.html'));
+
+  // })
+
+
+}
+// console.log("Subscribe");
+
+app.listen(PORT, () => {
+  console.log(`server is running on port no ${PORT}`);
+});
