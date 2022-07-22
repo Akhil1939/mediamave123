fd95069b2f1a5c910598c4919cfbc23502602d98
akhil1939 <akhilkukadiya464@gmail.com>
2022-07-22 19:23:22 +0530

    commit-7

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ if ( process.env.NODE_ENV == "production"){
 
   app.get("*", (req, res) => {
 
-      res.sendFile(path.resolve(__dirname, 'ui', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'ui', 'build', 'index.js'));
 
   })
 
